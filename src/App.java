00a3c252b730e102d8fa1266e7d145c1382e812c
VisnuRavi <visnu9016@gmail.com>
2022-12-08 19:53:08 +0800

    Set the barebones on the app

--- src/App.java
+++ src/App.java
@@ -1,2 +1,27 @@
+import Stage.Parser;
+import Stage.Stage;
+
+import java.util.Scanner;
+
 public class App {
+    public void run() {
+        initialiseMRTMap();
+        String input = getInput();
+        getShortestPath(input);
+    }
+
+    private String getInput() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+    private void getShortestPath(String input) {
+        Parser parser = Parser.getInstance();
+        parser.setInput(input);
+        parser.execute();
+    }
+
+    private void initialiseMRTMap() {
+        //get data from file and populate the stations and mrtmap
+    }
 }
